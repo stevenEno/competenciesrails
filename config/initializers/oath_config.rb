de83a81d7f25eda51b957ea8bc0b5b3cd4ba942c
Steven Eno <steven.e.eno@gmail.com>
2023-01-04 17:30:39 -0800

    cred fix

--- config/initializers/oath_config.rb
+++ config/initializers/oath_config.rb
@@ -1,10 +1,10 @@
 class OAuthConfig
     def self.client_id
-      '994253659361.4598605718917'
+      
     end
   
     def self.client_secret
-      '98c272bddebb042bff98806221d796f2'
+      
     end
 
     # SlackRubyBotServer.configure do |config|
