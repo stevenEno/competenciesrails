4972ce892ae04b36649e3943559670c937475b2f
Steven Eno <steven.e.eno@gmail.com>
2023-01-13 23:40:27 -0800

    simplified admin policy

--- app/policies/admin_policy.rb
+++ app/policies/admin_policy.rb
@@ -1,25 +1,4 @@
 class AdminPolicy < ApplicationPolicy
-    def initialize(user, professional)
-        @user = user
-        @professional = professional
-      end
-    
-    def index?
-      @user.admin?
-    end
-  
-    def show?
-      @user.admin?
-    end
-  
-    class Scope < Scope
-      def resolve
-        if user.admin?
-          scope.all
-        else
-          scope.none
-        end
-      end
-    end
-  end
+
+end
   
