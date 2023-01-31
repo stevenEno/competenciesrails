a30094f96331ad7d58dfa41fda72c82853c06b77
Steven Eno <steven.e.eno@gmail.com>
2023-01-31 10:52:49 -0800

    clean up

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_01_15_083330) do
+ActiveRecord::Schema[7.0].define(version: 2023_01_15_202614) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -86,6 +86,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_01_15_083330) do
     t.string "contact"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "is_visible"
   end
 
   create_table "portfolios", force: :cascade do |t|
